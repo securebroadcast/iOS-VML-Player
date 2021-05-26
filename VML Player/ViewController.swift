3ec8e2c4604c896ebb0feccdc34f600fdac84e81
Jordan Carroll <48207413+JordanSB@users.noreply.github.com>
2021-05-26 10:47:47 +0100

    Initial Commit

---
+++ VML Player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VML Player
+//
+//  Created by Jordan Carroll on 26/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ VML PlayerTests/VML_PlayerTests.swift
@@ -0,0 +1,33 @@
+//
+//  VML_PlayerTests.swift
+//  VML PlayerTests
+//
+//  Created by Jordan Carroll on 26/05/2021.
+//
+
+import XCTest
+@testable import VML_Player
+
+class VML_PlayerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
